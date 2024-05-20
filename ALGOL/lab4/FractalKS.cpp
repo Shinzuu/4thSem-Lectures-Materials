80b1b7e558778d1dadc87518b961678d2febb583
shinzuu <not.shinzuu@gmail.com>
2024-05-20 13:03:37 +0600

    with table and user input

--- ALGOL/lab4/FractalKS.cpp
+++ ALGOL/lab4/FractalKS.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 struct Object {
+    int index;
     double profit;
     double weight;
     double ratio;
@@ -13,40 +14,77 @@ bool compare(Object a, Object b) {
     return a.ratio > b.ratio;
 }
 
-double fractionalKnapsack(vector<double>& profit, vector<double>& weights, double capacity) {
-    int n = profit.size();
+double fractionalKnapsack(vector<double>& profit, vector<double>& weights, double capacity, int n) {
     vector<Object> objects(n);
 
-    // Initialize the objects with profit, weight, and their ratio
     for (int i = 0; i < n; ++i) {
+        objects[i].index = i; 
         objects[i].profit = profit[i];
         objects[i].weight = weights[i];
         objects[i].ratio = (double)profit[i] / weights[i];
     }
-    //compare based on ratio
-    sort(objects.begin(), objects.end(), compare);
 
     double maxProfit = 0.0;
+    vector<double> used(n, 0.0); //for outputting table
+
+    // Sort objects based on ratio
+    sort(objects.begin(), objects.end(), compare);
 
     for (int i = 0; i < n; ++i) {
+        int originalIndex = objects[i].index; 
         if (capacity > 0 && objects[i].weight <= capacity) {
             maxProfit += objects[i].profit;
+            used[originalIndex] = 1.0; 
             capacity -= objects[i].weight;
-        } else {
+        } else if (capacity > 0) {
             maxProfit += (capacity * objects[i].ratio);
+            used[originalIndex] = capacity / objects[i].weight;
+            capacity = 0;
+        } else {
             break;
         }
     }
-    
+
+    cout << "Items Used Table:" << endl;
+    cout << "Item\tUsed" << endl;
+    for (int i = 0; i < n; ++i) {
+
+        cout << objects[i].index + 1 << "\t";
+
+        if (used[objects[i].index] == 1.0) {
+            cout << "1" << endl;
+        } else {
+            cout << used[objects[i].index] << endl;
+        }
+    }
+
     return maxProfit;
 }
 
 int main() {
-    vector<double> profit = {10, 5, 15, 7, 6, 18, 3};
-    vector<double> weights = {2, 3, 5, 7, 1, 4, 1};
-    double capacity = 15;
+    int n;
+    double capacity;
 
-    cout << fixed << "Maximum value: " << fractionalKnapsack(profit, weights, capacity) << endl;
+    cout << "Enter the number of objects: ";
+    cin >> n;
+
+    vector<double> profit(n);
+    vector<double> weights(n);
+
+    cout << "Enter the profit each object:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> profit[i];
+    }
+
+    cout << "Enter the weight of each object:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> weights[i];
+    }
+
+    cout << "Enter the capacity of the knapsack: ";
+    cin >> capacity;
+
+    cout << fixed << "Maximum value: " << fractionalKnapsack(profit, weights, capacity, n) << endl;
 
     return 0;
 }
