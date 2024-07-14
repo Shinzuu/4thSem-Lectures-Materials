c09bcef9440a4e5af807471eb60c0ca9e3c7acef
shinzuu <not.shinzuu@gmail.com>
2024-07-14 19:33:16 +0600

    ans 2

--- DMSL/lab_perf2/prePract/index.php
+++ DMSL/lab_perf2/prePract/index.php
@@ -8,8 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
 
 <body>
@@ -19,24 +18,41 @@
     <main>
         <!-- place main content here -->
         <section class="container" style="padding: 20px; border: 3px dotted magenta;">
-            <h3>Adding Student Info</h3>
+            <h5>Adding Student Info</h5>
 
             <form action="add_student.php" method="post">
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control" name="name" id="name" aria-describedby="helpId"
-                        placeholder="John Doe" required />
+                    <input type="text" class="form-control" name="name" id="name" aria-describedby="helpId" placeholder="John Doe" required />
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId"
-                        placeholder="John@mail.com" required />
+                    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId" placeholder="John@mail.com" required />
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </section>
-        <section class="container" style ="padding: 20px; border: 3px dotted Pink;" >
-            
+        <section class="container" style="padding: 20px; border: 3px dotted Pink;">
+            <h5>List of Students</h5>
+
+            <div class="table-responsive">
+                <table class="table table-primary">
+                    <thead>
+                        <tr>
+                            <th scope="col">Student ID</th>
+                            <th scope="col">Student Name</th>
+                            <th scope="col">Student Email</th>
+                        </tr>
+                    </thead>
+                    <tbody id="student_table_row">
+                        <!-- Table row via dynamic PHP -->
+                        <?php
+                        include_once "show_student.php";
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+
         </section>
 
     </main>
@@ -44,13 +60,9 @@
         <!-- place footer here -->
     </footer>
     <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ DMSL/lab_perf2/prePract/show_student.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "practice";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM students";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr class='student_table_row'>";
+        echo "<td>" . $row["student_id"] . "</td>";
+        echo "<td>" . $row["name"] . "</td>";
+        echo "<td>" . $row["email"] . "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='3'>No data found</td></tr>";
+}
+
+$conn->close();
+?>
